76eca8d55dbb964eaffd6f4e6b56be13ebd624e1
DomePuma <mathys.mechain@gmail.com>
2024-11-22 11:27:37 +0100

    Add letter by letter + rich text compatibility

--- Assets/_Core/Scripts/DialogueController.cs
+++ Assets/_Core/Scripts/DialogueController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,7 +10,9 @@ public class DialogueController : MonoBehaviour
     [SerializeField] private TMP_Text _textNPC;
     [SerializeField] private Image _imagePlayer;
     [SerializeField] private Image _imageNPC;
+    [SerializeField] private float letterDelay = .05f;
 
+    
     private int dialogueIndex = 0;
 
     public void NextDialogue()
@@ -18,14 +21,14 @@ public class DialogueController : MonoBehaviour
         {
             if(dialogueScriptable.dialogueSection[dialogueIndex].isPlayer)
             {
-                _textPlayer.text = dialogueScriptable.dialogueSection[dialogueIndex].dialogue;
+                WriteText(dialogueScriptable.dialogueSection[dialogueIndex].dialogue, _textPlayer);
                 _imagePlayer.sprite = dialogueScriptable.dialogueSection[dialogueIndex].sprite;
                 _imagePlayer.preserveAspect = true;
                 dialogueIndex++;
             }
             else
             {
-                _textNPC.text = dialogueScriptable.dialogueSection[dialogueIndex].dialogue;
+                WriteText(dialogueScriptable.dialogueSection[dialogueIndex].dialogue, _textNPC);
                 _imageNPC.sprite = dialogueScriptable.dialogueSection[dialogueIndex].sprite;
                 _imageNPC.preserveAspect = true;
                 dialogueIndex++;
@@ -35,5 +38,21 @@ public class DialogueController : MonoBehaviour
         {
 
         }
-    } 
+    }
+
+    public void WriteText(string text, TMP_Text textBox)
+    {
+        textBox.maxVisibleCharacters = 0; //Resets whenever this is re-used.
+        textBox.text = text;
+        StartCoroutine(TypeText(textBox));
+    }
+
+    IEnumerator TypeText(TMP_Text textBox)
+    {
+        for (int i = 0; i < textBox.text.Length; i++)
+        {
+            textBox.maxVisibleCharacters++;
+            yield return new WaitForSeconds(letterDelay);
+        }
+    }
 }
