bb3f0f52dd03f28c26c12f99c4dfb34fc18f17cf
Logan <pix.59700@gmail.com>
2024-12-04 15:28:20 +0100

    Add abstrac Cooking machine to interact with object in hand

---
+++ Assets/_Core/Scripts/Item/ItemData.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Item")]
+public class ItemData : ScriptableObject
+{
+    [SerializeField] private string obejctName;
+    [SerializeField] private ElementType elementType;
+    [SerializeField, Range(1,4)] private int rarity;
+    [SerializeField] private ItemUsage itemUsage;
+    [SerializeField] private Sprite sprite;
+    [SerializeField] private ItemData itemToConvert;
+    [SerializeField] private string description;
+    
+    public string ObjectName
+    {
+        get => obejctName;
+    }
+
+    public ElementType Type
+    {
+        get => elementType;
+    }
+
+    public int Rarity
+    {
+        get => rarity;
+    }
+
+    public ItemUsage Usage
+    {
+        get => itemUsage;
+    }
+
+    public Sprite ItemSprite
+    {
+        get => sprite;
+    }
+    
+    public ItemData ItemToConvert
+    {
+        get => itemToConvert;
+    } 
+
+    private string Description
+    {
+        get => description;
+    }
+}
