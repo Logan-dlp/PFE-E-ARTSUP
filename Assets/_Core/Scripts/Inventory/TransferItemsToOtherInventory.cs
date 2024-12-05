38b7c8ed5e0d322913385e56b7af06b7361aeaff
LaPetiteDev <Lahessaimere@gmail.com>
2024-12-05 12:28:30 +0100

    Add Inventory Chest

--- Assets/_Core/Scripts/Inventory/TransferItemsToOtherInventory.cs
+++ Assets/_Core/Scripts/Inventory/TransferItemsToOtherInventory.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-public class TransferItemsToCellar : MonoBehaviour
+public class TransferItemsToOtherInventory : MonoBehaviour
 {
-    [SerializeField] private InventoryData _inventoryPlayer;
-    [SerializeField] private InventoryData _inventoryCellar;
+    [SerializeField] private InventoryData _inventorySends;
+    [SerializeField] private InventoryData _inventoryReceives;
 
     private InventoryUI _inventoryUIPlayer;
 
@@ -19,18 +19,18 @@ public class TransferItemsToCellar : MonoBehaviour
 
     public void SendItems()
     {
-        if (_inventoryPlayer == null || _inventoryCellar == null)
+        if (_inventorySends == null || _inventoryReceives == null)
         {
             Debug.LogWarning("Inventaire joueur ou cave non défini !");
             return;
         }
 
-        for (int i = _inventoryPlayer.Items.Count - 1; i >= 0; i--)
+        for (int i = _inventorySends.Items.Count - 1; i >= 0; i--)
         {
-            ItemData item = _inventoryPlayer.Items[i];
+            ItemData item = _inventorySends.Items[i];
 
-            _inventoryCellar.Items.Add(item);
-            _inventoryPlayer.Items.RemoveAt(i);
+            _inventoryReceives.Items.Add(item);
+            _inventorySends.Items.RemoveAt(i);
             Debug.Log("Ajout des items dans l'inventaire de Cellar !");
         }
 
