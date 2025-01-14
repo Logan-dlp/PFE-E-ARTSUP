066cd01d928ff550b39b34fb06b5eb0f22483cd9
DomePuma <mathys.mechain@gmail.com>
2025-01-14 16:30:12 +0100

    Probuilder + Spawn objects at random points + Base of Interactable Objects

---
+++ Assets/_Core/Scripts/ObjectSpawn/GetRandomPoint.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace MoonlitMixes.ObjectSpwan
+{
+    public class GetRandomPoint : MonoBehaviour
+    {
+        [SerializeField] private GameObject _prefabTest;
+        [SerializeField] private Vector2 _spawnMinMax; 
+    
+        private Mesh _mesh;
+        private Vector3[] _verticesArray;
+    
+        private void Start()
+        {
+            _mesh = GetComponent<MeshCollider>().sharedMesh;
+            _verticesArray = _mesh.vertices;
+
+            int nbOfSpawn = (int)Random.Range(_spawnMinMax.x, _spawnMinMax.y);
+        
+            for (int i = 0; i < nbOfSpawn; i++)
+            {
+                GameObject createdObject = Instantiate(_prefabTest);
+                createdObject.transform.position = gameObject.transform.position + _verticesArray[Random.Range(0, _verticesArray.Length)];
+            }
+        }
+    }
+}
