3694a4bb5f630a7d3f9f1ad9d976218cfcb6e1ae
DomePuma <mathys.mechain@gmail.com>
2025-01-15 15:03:08 +0100

    Add prefab array

--- Assets/_Core/Scripts/ObjectSpawn/GetRandomPoint.cs
+++ Assets/_Core/Scripts/ObjectSpawn/GetRandomPoint.cs
@@ -4,7 +4,7 @@ namespace MoonlitMixes.ObjectSpwan
 {
     public class GetRandomPoint : MonoBehaviour
     {
-        [SerializeField] private GameObject _prefabTest;
+        [SerializeField] private GameObject[] _prefabArray;
         [SerializeField] private Vector2 _spawnMinMax; 
     
         private Mesh _mesh;
@@ -19,7 +19,7 @@ namespace MoonlitMixes.ObjectSpwan
         
             for (int i = 0; i < nbOfSpawn; i++)
             {
-                GameObject createdObject = Instantiate(_prefabTest);
+                GameObject createdObject = Instantiate(_prefabArray[Random.Range(0,_prefabArray.Length)]);
                 createdObject.transform.position = gameObject.transform.position + _verticesArray[Random.Range(0, _verticesArray.Length)];
             }
         }
