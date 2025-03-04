3ab14556074ca295a2f7e666e25f86c2a971416a
DomePuma <mathys.mechain@gmail.com>
2025-03-04 17:16:32 +0100

    Spwaner modif + remove multiple obj in 1 spawn point + load obj from scriptable

--- Assets/_Core/Scripts/ObjectSpawn/GetRandomPoint.cs
+++ Assets/_Core/Scripts/ObjectSpawn/GetRandomPoint.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using MoonlitMixes.Datas;
+using UnityEditor;
 using UnityEngine;
 
 namespace MoonlitMixes.ObjectSpwan
@@ -6,22 +9,64 @@ namespace MoonlitMixes.ObjectSpwan
     {
         [SerializeField] private GameObject[] _prefabArray;
         [SerializeField] private Vector2 _spawnMinMax; 
-    
+        [SerializeField] private ObjectSpawnScriptableData _objectSpawnScriptableData;
+        
         private Mesh _mesh;
         private Vector3[] _verticesArray;
+        private List<Vector3> _usedVerticeList = new List<Vector3>();
     
         private void Start()
         {
-            _mesh = GetComponent<MeshCollider>().sharedMesh;
-            _verticesArray = _mesh.vertices;
-
-            int nbOfSpawn = (int)Random.Range(_spawnMinMax.x, _spawnMinMax.y);
-        
-            for (int i = 0; i < nbOfSpawn; i++)
+            if(_objectSpawnScriptableData.objectSpawneds.Length == 0)
             {
-                GameObject createdObject = Instantiate(_prefabArray[Random.Range(0,_prefabArray.Length)]);
-                createdObject.transform.position = gameObject.transform.position + _verticesArray[Random.Range(0, _verticesArray.Length)];
+                _mesh = GetComponent<MeshCollider>().sharedMesh;
+                _verticesArray = _mesh.vertices;
+
+                if(_verticesArray.Length < _spawnMinMax.y)
+                {
+                    Debug.LogError("Too much object compared to the number of vertices");
+                    
+                    #if UNITY_EDITOR
+                        EditorApplication.ExitPlaymode();
+                    #endif
+                }
+
+                int nbOfSpawn = (int)Random.Range(_spawnMinMax.x, _spawnMinMax.y);
+        
+                for (int i = 0; i < nbOfSpawn; i++)
+                {
+                    GameObject createdObject = Instantiate(_prefabArray[Random.Range(0,_prefabArray.Length)]);
+                    
+                    createdObject.transform.position = gameObject.transform.position + SelectRandomVertice();
+                }
             }
+            else
+            {
+                for (int i = 0; i < _objectSpawnScriptableData.objectSpawneds.Length; i++)
+                {
+                    Debug.Log("Loop");
+                    GameObject createdObject = Instantiate(_objectSpawnScriptableData.objectSpawneds[i]._object);
+                    createdObject.transform.position = _objectSpawnScriptableData.objectSpawneds[i]._verticesSpawnPosition;
+                }
+            }
+        }
+        
+        private Vector3 SelectRandomVertice()
+        {
+            bool isVerticeAvailable = false;
+            Vector3 vertice = new Vector3();
+
+            while(!isVerticeAvailable)
+            {
+                vertice = _verticesArray[Random.Range(0, _verticesArray.Length)];
+                if(!_usedVerticeList.Contains(vertice))
+                {
+                    _usedVerticeList.Add(vertice);
+                    isVerticeAvailable = true;
+                } 
+            }
+
+            return vertice;
         }
     }
 }
