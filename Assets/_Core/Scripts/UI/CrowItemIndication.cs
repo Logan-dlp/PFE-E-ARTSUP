8cb52e7d0746508e4dd428de7a7c224e6476f0a9
DomePuma <mathys.mechain@gmail.com>
2025-04-10 11:35:25 +0200

    Fix end anim

--- Assets/_Core/Scripts/UI/CrowItemIndication.cs
+++ Assets/_Core/Scripts/UI/CrowItemIndication.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using MoonlitMixes.Events;
 using MoonlitMixes.Item;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MoonlitMixes.UI
 {
@@ -12,6 +13,12 @@ namespace MoonlitMixes.UI
         [SerializeField] private ScriptableEvent _scriptableBubbleEvent;
         [SerializeField] private Animator _animatorImage;
         [SerializeField] private Animator _animatorBubble;
+        [SerializeField] private RectTransform _rectTransformBubble;
+        [SerializeField] private Image _imageAction;
+        [SerializeField] private Sprite _baseSprite;
+
+        private Vector3 _rectTransformBubblePosition;
+        private Vector2 _rectTransformBubbleSize;
 
         private void OnEnable()
         {
@@ -25,6 +32,12 @@ namespace MoonlitMixes.UI
             _scriptableItemUsageEvent.OnItemUsageEvent -= OpenUIAnim;
             _scriptableIndicationEvent.OnEvent -= CloseUIAnim;
             _scriptableBubbleEvent.OnEvent -= DesactivateUI;
+        }
+
+        private void Awake()
+        {
+            _rectTransformBubblePosition = _rectTransformBubble.localPosition;
+            _rectTransformBubbleSize = _rectTransformBubble.sizeDelta;
         }
 
         private void OpenUIAnim(ItemUsage itemUsage)
@@ -42,6 +55,7 @@ namespace MoonlitMixes.UI
 
         private void CloseUIAnim()
         {
+            _imageAction.sprite = _baseSprite;
             _animatorImage.gameObject.SetActive(false);
             _animatorBubble.SetTrigger("Close");
         }
@@ -49,6 +63,9 @@ namespace MoonlitMixes.UI
         private void DesactivateUI()
         {
             _animatorBubble.gameObject.SetActive(false);
+            
+            _rectTransformBubble.localPosition = _rectTransformBubblePosition; 
+            _rectTransformBubble.sizeDelta = _rectTransformBubbleSize;
         }
     }
 }
