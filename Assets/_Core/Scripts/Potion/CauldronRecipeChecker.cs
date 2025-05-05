9f41e97a44a564c4259526460e03da5eed5e287d
LaPetiteDev <lahessaimere@gmail.com>
2025-05-05 12:55:57 +0200

    Refactor(cauldron): prevent invalid ingredient at start + switch VFX to ParticleSystem.Play

--- Assets/_Core/Scripts/Potion/CauldronRecipeChecker.cs
+++ Assets/_Core/Scripts/Potion/CauldronRecipeChecker.cs
@@ -11,7 +11,8 @@ namespace MoonlitMixes.Potion
     public class CauldronRecipeChecker : MonoBehaviour
     {
         [SerializeField] private GameObject _interactUI;
-        [SerializeField] private GameObject _bubbleVFX;
+        [SerializeField] private ParticleSystem _bubbleVFX;
+        [SerializeField] private ParticleSystem _burnPot;
         [SerializeField] private List<Recipe> _allRecipes;
         [SerializeField] private List<ItemData> _currentIngredients = new List<ItemData>();
 
@@ -58,27 +59,32 @@ namespace MoonlitMixes.Potion
 
         public void AddIngredient(ItemData ingredient)
         {
-            if(!_currentIngredients.Any())
-            {
-                _cauldronTimer.TimerIsActive = true;
-                
-                foreach (Recipe recipe in _allRecipes)
-                {
-                    if(recipe.RequiredIngredients[0] == ingredient)
-                    {
-                        _currentRecipe = recipe;
-                        _currentRecipeIndex = 0;
-                        break;
-                    }
-                }
-            } 
-
             if (ingredient == null)
             {
                 return;
             }
-            
-            if(_currentRecipe == null || ingredient != _currentRecipe.RequiredIngredients[_currentRecipeIndex]) 
+
+            if (!_currentIngredients.Any())
+            {
+                foreach (Recipe recipe in _allRecipes)
+                {
+                    if (recipe.RequiredIngredients[0] == ingredient)
+                    {
+                        _currentRecipe = recipe;
+                        _currentRecipeIndex = 0;
+                        _cauldronTimer.TimerIsActive = true;
+                        break;
+                    }
+                }
+
+                if (_currentRecipe == null)
+                {
+                    TriggerBurnPot();
+                    return;
+                }
+            }
+
+            if (_currentRecipe == null || ingredient != _currentRecipe.RequiredIngredients[_currentRecipeIndex])
             {
                 HandleFailedPotion();
                 return;
@@ -158,12 +164,29 @@ namespace MoonlitMixes.Potion
             _ingredentToAdd = null;
         }
 
+        private void TriggerBurnPot()
+        {
+            if (_burnPot != null)
+            {
+                _burnPot.Play();
+                Invoke(nameof(DisableBurnPot), _burnPot.main.duration);
+            }
+        }
+
+        private void DisableBurnPot()
+        {
+            if (_burnPot != null)
+            {
+                _burnPot.Stop();
+            }
+        }
+
         private void TriggerBubbleVFX()
         {
             if (_bubbleVFX != null)
             {
-                _bubbleVFX.SetActive(true);
-                Invoke(nameof(DisableBubbleVFX), _cauldronTimer.RemainingTime);
+                _bubbleVFX.Play();
+                Invoke(nameof(DisableBubbleVFX), _bubbleVFX.main.duration);
             }
         }
 
@@ -171,7 +194,7 @@ namespace MoonlitMixes.Potion
         {
             if (_bubbleVFX != null)
             {
-                _bubbleVFX.SetActive(false);
+                _bubbleVFX.Stop();
             }
         }
 
