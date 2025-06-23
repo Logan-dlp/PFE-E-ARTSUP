3f1ab3981e74b9242ade93c4a870512a207302bd
Theo <vicini.tv@gmail.com>
2025-06-23 15:29:17 +0200

    Add decal hit on monsters- Enemy Health
    Enemy Health : activate the shaders of monster damaged when a monster is hit
                   create the function to activate the vfx of die --> wait the vfx to integrate in prefabs

--- Assets/_Core/Scripts/Health/EnemyHealth.cs
+++ Assets/_Core/Scripts/Health/EnemyHealth.cs
@@ -1,19 +1,62 @@
+using NaughtyAttributes;
+using System.Collections;
 using UnityEngine;
 
 namespace MoonlitMixes.Health
 {
     public class EnemyHealth : AHealth
     {
+        [SerializeField] Material _hitMaterial;
+        [SerializeField] float _timeHitVFX;
+        [SerializeField] GameObject _dieVFX;
+        [SerializeField] float _timeDieVFX;
+        private bool _hit = false;
+        private float _time = 1;
         public override void TakeDamage(float damage)
         {
             RemoveHealth(damage);
+            _hit = true;
+            _time = 1;
+            if (damage > 0) StartCoroutine(ActivateHitVFX());
         }
+        [Button]
+        public void  Test ()
+        {
+            _hit = true;
+            _time = 1;
+            StartCoroutine(ActivateHitVFX());
+        }
+        IEnumerator ActivateHitVFX ()
+        {
+            yield return new WaitForSeconds(_timeHitVFX / 100);
 
+            if (_hit)
+            {
+                if (_time <= 0) { _hit = false; StartCoroutine(ActivateHitVFX());}
+                else if (_time > 0)
+                {
+                    _time -= 0.05f;
+                    _hitMaterial.SetFloat("_Split", _time);
+                    StartCoroutine(ActivateHitVFX());
+                }
+            }
+            else if (!_hit)
+            {
+                if (_time <= 1)
+                {
+                    _time += 0.05f;
+                    _hitMaterial.SetFloat("_Split", _time);
+                    StartCoroutine(ActivateHitVFX());
+                }
+                else _hit = true;
+            }
+        }
         protected override void CheckHealth()
         {
             if(_currentHealth <= 0)
             {
                 Debug.Log("EnemyDead");
+                if(_dieVFX!=null) _dieVFX.SetActive(true);
             }
             
             healthBarScriptableInt.SendHealthAmount(_currentHealth / _maxHealth);
