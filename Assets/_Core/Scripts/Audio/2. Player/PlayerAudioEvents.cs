41d444c0703074badb18fd6c26414d55dc0eff20
LaPetiteDev <lahessaimere@gmail.com>
2025-06-24 12:01:16 +0200

    Add sound on all scene shop

--- Assets/_Core/Scripts/Audio/2. Player/PlayerAudioEvents.cs
+++ Assets/_Core/Scripts/Audio/2. Player/PlayerAudioEvents.cs
@@ -44,7 +44,7 @@ public class PlayerAudioEvents : MonoBehaviour
         {
             RuntimeManager.StudioSystem.setParameterByName("FootstepsLocalisation", 1f);
         }
-        else if (currentSceneName == "S_Shop")
+        else if (currentSceneName == "S_Shop_Morning" || currentSceneName == "S_Shop_Twilight")
         {
             RuntimeManager.StudioSystem.setParameterByName("FootstepsLocalisation", 2f);
         }
