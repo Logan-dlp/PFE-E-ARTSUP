060016d31db06665ec787904e0eef1c35a9ed1d4
LaPetiteDev <lahessaimere@gmail.com>
2025-06-24 16:50:18 +0200

    spawn the client on the right day

--- Assets/_Core/Scripts/PNJ/CustomerSpawner.cs
+++ Assets/_Core/Scripts/PNJ/CustomerSpawner.cs
@@ -3,6 +3,7 @@ using MoonlitMixes.AI.PNJ.StateMachine.States;
 using MoonlitMixes.Datas;
 using MoonlitMixes.Player;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,40 +17,61 @@ namespace MoonlitMixes.AI.PNJ.Spawner
         [SerializeField] private List<GameObject> _pnjPrefabs;
         [SerializeField] private Transform _spawnPoint;
         [SerializeField] private float _timeBetweenSpawns = 2f;
-        [SerializeField] private int _maxCustomers = 3;
         [SerializeField] private PlayerMovement _playerMovement;
         [SerializeField] private DayNightCycleInfo _dayNightCycleInfo;
 
+        private readonly Dictionary<int, List<int>> _dayToPNJIndices = new Dictionary<int, List<int>>
+        {
+            { 0, new List<int> { 0, 1 } },
+            { 1, new List<int> { 2, 3, 4 } },
+            { 2, new List<int> { 5, 6, 7, 8 } }
+        };
+
+        private List<int> _currentDayPNJIndices;
         private int _currentPNJIndex = 0;
         private bool _isSpawning = false;
 
         public void StartSpawning()
         {
-            if (!_isSpawning)
+            if (_isSpawning) return;
+
+            _isSpawning = true;
+            _currentPNJIndex = 0;
+            _playerMovement.BlockMovement(true);
+
+            int currentDay = _dayNightCycleInfo.ActualDay;
+            if (_dayToPNJIndices.TryGetValue(currentDay, out _currentDayPNJIndices))
             {
-                _isSpawning = true;
-                _currentPNJIndex = 0;
-                _playerMovement.BlockMovement(true);
-
-
                 SpawnNextPNJ();
+            }
+            else
+            {
+                Debug.LogWarning($"Aucun PNJ assigné pour le jour {currentDay}");
+                NotifyAllCustomersGone();
             }
         }
 
         private void SpawnNextPNJ()
         {
-            if (_currentPNJIndex < _pnjPrefabs.Count)
+            if (_currentPNJIndex < _currentDayPNJIndices.Count)
             {
                 StartCoroutine(PlayBellAndSpawn());
             }
         }
 
-        private IEnumerator<WaitForSeconds> PlayBellAndSpawn()
+        private IEnumerator PlayBellAndSpawn()
         {
             OnClientBellRequested?.Invoke();
             yield return new WaitForSeconds(0.5f);
 
-            GameObject pnjInstance = _pnjPrefabs[_currentPNJIndex];
+            int pnjIndex = _currentDayPNJIndices[_currentPNJIndex];
+            if (pnjIndex < 0 || pnjIndex >= _pnjPrefabs.Count)
+            {
+                Debug.LogError($"PNJ index {pnjIndex} invalide dans la liste _pnjPrefabs");
+                yield break;
+            }
+
+            GameObject pnjInstance = _pnjPrefabs[pnjIndex];
             pnjInstance.transform.position = _spawnPoint.position;
             pnjInstance.SetActive(true);
 
@@ -62,6 +84,31 @@ namespace MoonlitMixes.AI.PNJ.Spawner
             }
 
             _currentPNJIndex++;
+        }
+
+        private IEnumerator WaitAndSpawnNext()
+        {
+            yield return new WaitForSeconds(_timeBetweenSpawns);
+            SpawnNextPNJ();
+        }
+
+        private void OnPNJDespawned()
+        {
+            if (_currentPNJIndex < _currentDayPNJIndices.Count)
+            {
+                StartCoroutine(WaitAndSpawnNext());
+            }
+            else
+            {
+                NotifyAllCustomersGone();
+            }
+        }
+
+        private void NotifyAllCustomersGone()
+        {
+            _dayNightCycleInfo.ActualTimePhase++;
+            _playerMovement.BlockMovement(false);
+            _isSpawning = false;
         }
 
         private void OnEnable()
@@ -77,31 +124,6 @@ namespace MoonlitMixes.AI.PNJ.Spawner
         public static void RequestSpawning()
         {
             OnStartSpawningRequested?.Invoke();
-        }
-
-        private IEnumerator<WaitForSeconds> WaitAndSpawnNext()
-        {
-            yield return new WaitForSeconds(_timeBetweenSpawns);
-            SpawnNextPNJ();
-        }
-
-        private void OnPNJDespawned()
-        {
-            if (_currentPNJIndex < _pnjPrefabs.Count)
-            {
-                StartCoroutine(WaitAndSpawnNext());
-            }
-            else
-            {
-                NotifyAllCustomersGone();
-            }
-        }
-
-        private void NotifyAllCustomersGone()
-        {
-            _dayNightCycleInfo.ActualTimePhase++;
-            _playerMovement.BlockMovement(false);
-            _isSpawning = false;
         }
     }
 }
