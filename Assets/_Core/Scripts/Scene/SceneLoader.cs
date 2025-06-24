09989c43fe04ad31eb42b1d487c3ba51dd025e1f
DomePuma <mathys.mechain@gmail.com>
2025-06-24 22:09:26 +0200

    Start Tutorial Forest

--- Assets/_Core/Scripts/Scene/SceneLoader.cs
+++ Assets/_Core/Scripts/Scene/SceneLoader.cs
@@ -32,7 +32,7 @@ namespace MoonlitMixes.Scene
                     _sceneToLoad.allowSceneActivation = true;
                 }
 
-                yield return null;
+                yield return new WaitForEndOfFrame();
             }
         }
 
--- Assets/_Core/Scripts/Tutorial/TutorialActivation.cs
+++ Assets/_Core/Scripts/Tutorial/TutorialActivation.cs
@@ -27,11 +27,14 @@ namespace MoonlitMixes.Tutorial
                 case EnumScene.ShopMorning:
                     if (!_tutorialSaveInfo.tutorialHubDone && _lastSceneNameData.sceneName == "S_TitleScreen")
                     {
-                        Debug.Log("Tuto");
                         TutorialShopMorningPart1();
                     }
                     break;
                 case EnumScene.Forest:
+                    if (!_tutorialSaveInfo.tutorialHubDone)
+                    {
+                        TutorialForestPart1();
+                    }
                     break;
                 case EnumScene.Labo:
                     break;
@@ -64,14 +67,19 @@ namespace MoonlitMixes.Tutorial
             _tutorialSaveInfo.tutorialShopMorningDone = true;
         }
 
-        private void TutorialHub()
+        private void TutorialForestPart1()
         {
-
+            _outlineToActivate[1].SetActive(true);
+            scriptableEvent2.OnEvent += TutorialForestPart2;
+            DialogueController.Instance.StartDialogue(dialogueDatasArray[0]);
+            _outlineToActivate[1].GetComponentInParent<PickUpTool>().canToolPickedUp = true;
         }
 
-        private void TutorialForest()
+        private void TutorialForestPart2()
         {
-
+            _outlineToActivate[1].SetActive(false);
+            scriptableEvent2.OnEvent -= TutorialForestPart2;
+            DialogueController.Instance.StartDialogue(dialogueDatasArray[1]);
         }
 
         private void TutorialLabo()
