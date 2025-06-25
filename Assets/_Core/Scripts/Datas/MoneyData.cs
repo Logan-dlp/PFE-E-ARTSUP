300e7ff64b769a7e4c46a40c8769a134e6c87b6b
DomePuma <mathys.mechain@gmail.com>
2025-06-25 02:46:35 +0200

    Finish Tuto sell, but customers have a problem

---
+++ Assets/_Core/Scripts/Datas/MoneyData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace MoonlitMixes.Datas
+{
+    [CreateAssetMenu(fileName = "MoneyData", menuName = "Scriptable Objects/MoneyData")]
+    public class MoneyData : ScriptableObject
+    {
+        public int money;
+    }
+}
--- Assets/_Core/Scripts/Tutorial/EnumScene.cs
+++ Assets/_Core/Scripts/Tutorial/EnumScene.cs
@@ -6,5 +6,6 @@ namespace MoonlitMixes.Tutorial
         Forest,
         Labo,
         ShopTwilight,
+        Cave,
     }
 }
